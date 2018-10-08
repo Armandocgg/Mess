7fb073a670095398129ada4302ab709aa7b15db1
Armando Guzman <armando@ideamosweb.com>
2018-10-07 22:11:59 -0500

    TabBottom

--- src/navigation/BottomTabNavigation.js
+++ src/navigation/BottomTabNavigation.js
@@ -14,6 +14,19 @@ const PeopleTabIcon = ({ tintColor }) => (
   <Ionicons name="ios-people" size={20} color={tintColor} />
 );
 
+const CameraTabIcon = ({ tintColor }) => (
+  <Ionicons name="ios-camera" size={20} color={tintColor} />
+);
+
+const GameTabIcon = ({ tintColor }) => (
+  <Ionicons name="ios-people" size={20} color={tintColor} />
+);
+
+const ImagenTabIcon = ({ tintColor }) => (
+  <Ionicons name="ios-image" size={20} color={tintColor} />
+);
+
+
 export const BottomTabNavigation = createBottomTabNavigator(
   {
     HomeScreen: {
@@ -29,10 +42,31 @@ export const BottomTabNavigation = createBottomTabNavigator(
         header: null,
         tabBarIcon: PeopleTabIcon
       }
+    },
+    CamaraScreen:{
+      screen: PeopleScreen,
+      navigationOptions: {
+        header: null,
+        tabBarIcon: CameraTabIcon
+      }
+    },
+    GameScreen:{
+      screen: PeopleScreen,
+      navigationOptions: {
+        header: null,
+        tabBarIcon: GameTabIcon
+      }
+    },
+    PhotoScreen:{
+      screen: PeopleScreen,
+      navigationOptions: {
+        header: null,
+        tabBarIcon: ImagenTabIcon
+      }
     }
   },
   {
-    tabBarOption: {
+    tabBarOptions: {
       showLabel: false,
       activeTintColor: '#0084ff',
       inactiveTintColor: '#626262',
